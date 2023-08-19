42f411196391d80483b71dfb2257a80d04bbc769
Jessica Seery <Jessica13.Seery@gmail.com>
2023-08-19 14:18:12 -0400

    adding components pt2

---
+++ src/components/Projects.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+import React from 'react'
+import menu from './menu.png'
+import himym from './HIMYM.PNG'
+
+const Projects = () => {
+    return (
+<div>
+    <p>project page... insert pics here</p>
+    <div>
+        <img src={menu} alt="menu"/>
+    </div>
+</div>
+    )
+}
+export default Projects
